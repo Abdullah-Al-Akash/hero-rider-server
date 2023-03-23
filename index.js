7d49096e97663a87ad076f947582c6f95f0052f0
Abdullah Al Akash <aabdullahalakash@gmail.com>
2023-03-23 19:09:00 +0600

    Complete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require("stripe")('sk_test_51If57xGFVg3TaXfKuw47AkD19lHzT20l991LTsUuW1Y1dxOVWM8r0Zv94YmYDcFR2Vdbaha26JibWSXFw02Lxr9n008d8slWGg');
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -26,10 +27,17 @@ async function run() {
 
                 // Load All Members:
                 app.get('/members', async (req, res) => {
-                        const query = {};
-                        const cursor = memberCollection.find(query);
-                        const members = await cursor.toArray();
-                        res.send(members);
+                        const page = parseInt(req.query.page);
+                        const size = parseInt(req.query.size);
+                        const cursor = memberCollection.find({});
+                        let members;
+                        if (page || size) {
+                                members = await cursor.skip(page * size).limit(size).toArray();
+                        }
+                        else {
+                                members = await cursor.toArray();
+                        }
+                        res.send(members)
                 })
 
                 // Login User
@@ -95,6 +103,24 @@ async function run() {
                         };
                         const result = await memberCollection.updateOne(filter, updateDoc, options);
                         res.json(result)
+                });
+
+                // For Payment:
+                app.post('/create-payment-intent', async (req, res) => {
+                        const money = req.body;
+                        const price = money.price;
+                        const amount = price * 100;
+
+                        const paymentIntent = await stripe.paymentIntents.create({
+                                currency: 'usd',
+                                amount: amount,
+                                "payment_method_types": [
+                                        "card"
+                                ]
+                        });
+                        res.send({
+                                clientSecret: paymentIntent.client_secret,
+                        });
                 });
         }
         finally {
